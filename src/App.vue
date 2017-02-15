235efa7354c2292c718d9423ee34c3e10ee81ff6
alexisbarnes <alexis.barnes06@gmail.com>
2017-02-15 12:21:15 -0500

    Added timeline as a component.

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,7 @@
     <!-- <Menu></Menu> -->
     <!-- This is the page navigation -->
     <div class="col-md-2">
-      <div class="timeline">
-        <ul>
-            <li v-for="game in allGames" :game="game">
-              <div>
-              </div>
-              <p class="menu-year"> {{ game.Year }} </p>
-            </li>
-        </ul>
-      </div>
+      <Timeline v-for="game in allGames" :game="game"></Timeline>
     </div>
 
     <div class="col-md-10">
@@ -23,12 +15,12 @@
 <script>
 import axios from 'axios'
 import Game from './components/Game'
-import Menu from './components/Menu'
+import Timeline from './components/Timeline'
 export default {
   name: 'app',
   components: {
     Game,
-    Menu
+    Timeline
   },
   data () {
     return {
@@ -48,48 +40,5 @@ export default {
 <style>
 #app {
 }
-*,
-*::before,
-*::after {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-body {
-  font: normal 16px/1.5 "Helvetica Neue", sans-serif;
-  background: #fff;
-  color: #000000;
-  overflow-x: hidden;
-  padding-bottom: 50px;
-}
-/* TIMELINE */
-.timeline ul li {
-  list-style-type: none;
-  position: relative;
-  width: 6px;
-  margin: 0 auto;
-  padding-top: 50px;
-  background: #000000;
-}
-.timeline ul li:hover {
-  background: #D5002B;
-}
-.timeline ul .menu-year:hover {
-  color: #D5002B;
-}
-.timeline ul li::after {
-  content: '';
-  position: absolute;
-  left: 50%;
-  bottom: 0;
-  transform: translateX(-50%);
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  background: inherit;
-}
-.menu-year {
-    margin: -25px 0 0px 20px;
-    float: left;
-}
+
 </style>
---
+++ src/components/Timeline.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="timeline">
+    <ul>
+        <li>
+          <div>
+          </div>
+          <p class="menu-year"> {{ game.Year }} </p>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'game'
+  ],
+  data () {
+    return {
+      allGames: []
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font: normal 16px/1.5 "Helvetica Neue", sans-serif;
+  background: #fff;
+  color: #000000;
+  overflow-x: hidden;
+  padding-bottom: 50px;
+}
+/* TIMELINE */
+.timeline ul li {
+  list-style-type: none;
+  position: relative;
+  width: 6px;
+  margin: 0 auto;
+  padding-top: 50px;
+  background: #000000;
+}
+.timeline ul li:hover {
+  background: #D5002B;
+}
+.timeline ul .menu-year:hover {
+  color: #D5002B;
+}
+.timeline ul li::after {
+  content: '';
+  position: absolute;
+  left: 50%;
+  bottom: 0;
+  transform: translateX(-50%);
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background: inherit;
+}
+.menu-year {
+    margin: -25px 0 0px 20px;
+    float: left;
+}
+
+</style>
